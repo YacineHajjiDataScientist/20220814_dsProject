39c90e4201ae62d1f173d824a80630adc3e1a0a0
YacineHajjiDataScientist <111184319+YacineHajjiDataScientist@users.noreply.github.com>
2022-09-02 23:51:51 +0200

    Add files via upload

--- 20220821_projectDS.py
+++ 20220821_projectDS.py
@@ -24,10 +24,17 @@ time
 [i.replace(":", "") for i in time['a']]
 
 
-# In[85]:
+# In[13]:
 
 
-dfCarac['hrmn'].replace(':', '')
+a=['07:05']
+a.replace(':', '')
+
+
+# In[11]:
+
+
+dfCarac['hrmn'].astype('object').replace(':', '')
 # [i.replace(":", "") for i in dfCarac['hrmn']]
 
 
@@ -104,7 +111,7 @@ print('dfCarac dimensions:', dfCarac.shape)
 
 # # Data-management
 
-# In[33]:
+# In[168]:
 
 
 # Computing date variable
@@ -120,7 +127,11 @@ dfCarac['mois_label'] = dfCarac['mois_label'].replace(to_replace=np.arange(1, 13
 dfCarac['weekday'] = dfCarac['date'].dt.weekday
 
 # Hour of the day
+dfCarac['hrmn'] = dfCarac['hrmn'].replace('\:', '', regex=True).astype(int)
 dfCarac['hour'] = dfCarac['hrmn']//100
+
+# Year of accident
+dfCarac['year'] = dfCarac['date'].dt.year
 
 
 # In[13]:
@@ -129,11 +140,10 @@ dfCarac['hour'] = dfCarac['hrmn']//100
 dfCarac['date'].value_counts().sort_index()
 
 
-# In[48]:
+# In[169]:
 
 
-
-dfCarac['hrmn'].value_counts()
+dfCarac['year'].value_counts().sort_index()
 
 
 # # Descriptive statistics
@@ -242,65 +252,135 @@ dfVehicules.head(3)
 # ### Graphs
 # ##### Time-related graphs
 
-# In[56]:
+# In[90]:
 
 
-fig, ax = plt.subplots(2, 1)
-sns.countplot(dfCarac['mois'], 
-             palette=['#D7F1F5', '#D7F1F5', 
-                   '#E3F5D7', '#E3F5D7', '#E3F5D7', 
-                   '#FEE7B9', '#FEE7B9', '#FEE7B9', 
-                   '#FEBEB9', '#FEBEB9', '#FEBEB9', 
-                   '#D7F1F5'], ax=ax[0])
-sns.countplot(x=dfCarac['mois'], hue=dfCarac['grav'], 
-             palette=['#F45050','#F4B650','#C8C8C8'], ax=ax[1]);
+# Gravity variable in Carac dataframe
+pd.DataFrame({'prop':dfCarac.grav.value_counts(normalize=True),
+              'count':dfCarac.grav.value_counts()})
 
 
-# In[87]:
+# In[89]:
 
 
+# Gravity variable in Lieux dataframe
+pd.DataFrame({'prop':dfLieux.grav.value_counts(normalize=True),
+              'count':dfLieux.grav.value_counts()})
+
+
+# $\color{#0005FF}{\text{Both dataframes Carac and Lieux have the same amount of accidents, they also have the same accident gravity distribution}}$
+
+# ### Year
+
+# In[170]:
+
+
+# Display plots
+plt.figure(figsize=(10, 4))
+sns.countplot(dfCarac['year'], palette=['#9D9D9D'])
+plt.hlines(y=len(dfCarac['year'])/16, xmin=-0.5, xmax=15.5, color='blue', alpha=0.4);
+# It seems that the number of accident never stops decreasing year after year
+# The observable large decreases seem to be during 2007-2008, 2011-2012 and 2019-2020
+# The number of accident seemed to be stable between 2013 and 2019
+
+
+# In[171]:
+
+
+# Initiating dataframe grouped by month
+dfCaracGpByYear = (dfCarac.groupby(['year'])['grav']
+                     .value_counts(normalize=True)
+                     .rename('percentage')
+                     .mul(100)
+                     .reset_index()
+                     .sort_values('grav'))
+
+# Display plotx
+fig, ax = plt.subplots(figsize=(10, 4))
+sns.barplot(x="year", y="percentage", hue="grav", data=dfCaracGpByYear, 
+             palette=['#F45050','#F4B650','#C8C8C8']);
+# It seems that the gravity is less important during 2018 to 2020
+
+
+# In[172]:
+
+
+# data-management
+dfYearGrav = pd.crosstab(dfCarac['year'], dfCarac['grav'], normalize=0).sort_values(by=2, ascending=False)
+
+# Display plots
+fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+sns.heatmap(dfYearGrav, annot=True, cmap='cubehelix', ax=ax[0])
+sns.heatmap(dfYearGrav.div(dfYearGrav.max(axis=0), axis=1), annot=True, cmap='magma_r', ax=ax[1]);
+fig.show()
+# Even though 2018 to 2020 have the largest proportions of accident gravity 3, they also have the lowest gravity 3 ones
+# It seems that the state has focused on reducing the overall number of accident but not the gravity of accidents
+
+
+# ### Months
+
+# In[151]:
+
+
+# Display plots
 plt.figure(figsize=(10, 4))
 sns.countplot(dfCarac['mois'], 
-             palette=['#D7F1F5', '#D7F1F5', 
-                   '#E3F5D7', '#E3F5D7', '#E3F5D7', 
-                   '#FEE7B9', '#FEE7B9', '#FEE7B9', 
-                   '#FEBEB9', '#FEBEB9', '#FEBEB9', 
-                   '#D7F1F5'])
-plt.hlines(y=len(dfCarac['mois'])/12, xmin=0, xmax=11, color='blue', alpha=0.4);
+             palette=['#96CED7', '#96CED7', 
+                   '#96D7A2', '#96D7A2', '#96D7A2', 
+                   '#D79696', '#D79696', '#D79696', 
+                   '#D7CF96', '#D7CF96', '#D7CF96', 
+                   '#96CED7'])
+plt.hlines(y=len(dfCarac['mois'])/12, xmin=-0.5, xmax=11.5, color='blue', alpha=0.4);
 # On peut observer que les mois de juin, juillet, septembre et octobre semblent avoir le plus d'accidents
 # On peut observer que le mois de février compte le moins d'accidents mais il comporte aussi 28 jours
 
 
-# In[93]:
+# In[152]:
 
 
-plt.figure(figsize=(10, 4))
-sns.countplot(x=dfCarac['mois'], hue=dfCarac['grav'], 
+# Initiating dataframe grouped by month
+dfCaracGpByMonth = (dfCarac.groupby(['mois'])['grav']
+                     .value_counts(normalize=True)
+                     .rename('percentage')
+                     .mul(100)
+                     .reset_index()
+                     .sort_values('grav'))
+
+# Display plotx
+fig, ax = plt.subplots(figsize=(10, 4))
+sns.barplot(x="mois", y="percentage", hue="grav", data=dfCaracGpByMonth, 
              palette=['#F45050','#F4B650','#C8C8C8']);
-
-# Faire un graphique où on affiche la proportion de gravité par mois
-# idem par heures du jour (24h au total)
+# It seems that the gravity of accident is larger during the weekend compared to the week
 
 
-# In[31]:
+# In[146]:
+
+
+# data-management
+dfMonthGrav = pd.crosstab(dfCarac['mois'], dfCarac['grav'], normalize=0).sort_values(by=2, ascending=False)
+
+# Display plots
+fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+sns.heatmap(dfMonthGrav, annot=True, cmap='cubehelix', ax=ax[0])
+sns.heatmap(dfMonthGrav.div(dfMonthGrav.max(axis=0), axis=1), annot=True, cmap='magma_r', ax=ax[1]);
+fig.show()
+# It seems that the largest proportion of accident gravity 2 & 3 happen during august and july
+
+
+# ### Weekday
+
+# In[86]:
 
 
 sns.countplot(x=dfCarac['weekday'], 
              palette=['#A0A491', '#A0A491', '#A0A491', '#A0A491', '#A0A491', '#E17441', '#E17441'])
-plt.hlines(y=len(dfCarac['weekday'])/7, xmin=0, xmax=6, color='blue', alpha=0.4);
+plt.hlines(y=len(dfCarac['weekday'])/7, xmin=-0.5, xmax=6.5, color='blue', alpha=0.4);
 plt.xticks(np.arange(7), ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'])
 plt.title("Nombre d'accident par jour de la semaine");
-
 # It seems that the friday is the accident day
 
 
-# In[127]:
-
-
-pd.crosstab(dfCarac['weekday'], dfCarac['grav'], normalize=0)
-
-
-# In[142]:
+# In[67]:
 
 
 # Initiating dataframe grouped by weekday
@@ -319,36 +399,66 @@ ax.set_xticklabels(['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi',
 # It seems that the gravity of accident is larger during the weekend compared to the week
 
 
-# In[146]:
+# In[154]:
 
 
-sns.histplot(dfCarac, x="weekday", hue="grav", stat="probability", multiple="fill", shrink=8);
+# data-management
+dfWeekdayGrav = pd.crosstab(dfCarac['weekday'], dfCarac['grav'], normalize=0).sort_values(by=2, ascending=False)
+
+# Display plots
+fig, ax = plt.subplots(1, 2, figsize=(10, 4))
+sns.heatmap(dfWeekdayGrav, annot=True, cmap='cubehelix', ax=ax[0])
+sns.heatmap(dfWeekdayGrav.div(dfWeekdayGrav.max(axis=0), axis=1), annot=True, cmap='magma_r', ax=ax[1]);
+fig.show()
+# Actually, the largest proportion of accident gravity 2 is during sunday then saturday
 
 
-# In[32]:
+# ### Hour of the day
+
+# In[87]:
 
 
-plt.figure(figsize=(10, 4))
-sns.countplot(x=dfCarac['weekday'], hue=dfCarac['grav'], 
+sns.countplot(x=dfCarac['hour'], 
+             palette=['#090F23', '#03060F', '#040D29', '#484743', '#999588', '#CDC5A9', 
+                     '#DDD3B0', '#F5E7B1', '#FFECA4', '#FFDEA4', '#FFD2A4', '#FFCBA4', 
+                     '#FFBDA4', '#FFAFA4', '#FFA4A4', '#FFA4D6', '#EDA4FF', '#C2A4FF', 
+                     '#A4AAFF', '#839AE5', '#5B71B8', '#3C508F', '#26366A', '#152043'])
+plt.xticks([0, 3, 6, 9, 12, 15, 18, 21], ['Minuit', '3h', '6h', '9h', 'Midi', '15h', '18h', '21h'])
+plt.title("Nombre d'accident par heure de la journée")
+plt.hlines(y=len(dfCarac['hour'])/24, xmin=-0.5, xmax=23.5, color='blue', alpha=0.4);
+# It seems that most accident happen between 4pm and 7pm which is when people usually go back home and the sun goes down
+# At 7am, the number of accident drastically increase and really goes down after 8pm
+
+
+# In[50]:
+
+
+# Initiating dataframe grouped by hour
+dfCaracGpByHour = (dfCarac.groupby(['hour'])['grav']
+                     .value_counts(normalize=True)
+                     .rename('percentage')
+                     .mul(100)
+                     .reset_index()
+                     .sort_values('grav'))
+
+# Display plotx
+fig, ax = plt.subplots(figsize=(10, 4))
+sns.barplot(x="hour", y="percentage", hue="grav", data=dfCaracGpByHour, 
              palette=['#F45050','#F4B650','#C8C8C8']);
+# Wow, it seems that the gravity of accidents is worst during the night (22pm-6am)
+# More than 5% gravity 2 during the night against less than 4% during full day
 
 
-# In[ ]:
+# In[157]:
 
 
-sns.countplot(x=dfCarac['hour'])
-plt.xticks([0, 6, 12, 18, 24], ['Minuit', '6h', 'Midi', '18h', 'Minuit'])
-plt.title("Nombre d'accident par heure de la journée");
+# data-management
+dfHourGrav = pd.crosstab(dfCarac['hour'], dfCarac['grav'], normalize=0).sort_values(by=2, ascending=False)
 
-
-# In[89]:
-
-
-dfCarac['hrmn']
-
-
-# In[5]:
-
-
-dfLieux.grav.value_counts()
+# Display plots
+fig, ax = plt.subplots(1, 2, figsize=(10, 8))
+sns.heatmap(dfHourGrav, annot=True, cmap='cubehelix', ax=ax[0])
+sns.heatmap(dfHourGrav.div(dfHourGrav.max(axis=0), axis=1), annot=True, cmap='magma_r', ax=ax[1]);
+fig.show()
+# Proposition: creating a full night variable [0-6am]
 
