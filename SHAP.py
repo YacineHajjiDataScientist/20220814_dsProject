5bdf28492ff6d7d643b9495ab4d5930c68a9686d
YacineHajjiDataScientist <111184319+YacineHajjiDataScientist@users.noreply.github.com>
2022-12-08 20:56:38 +0100

    Add files via upload

--- SHAP.py
+++ SHAP.py
@@ -13,6 +13,8 @@ pip install shap
 ### Import libraries
 import pandas as pd
 
+from sklearn.preprocessing import StandardScaler
+
 from sklearn import model_selection
 import xgboost as xgb
 
@@ -32,10 +34,20 @@ print(feature_matrix.shape)
 print(target.shape)
 
 
-# In[3]:
+# In[23]:
 
 
+### Train/test split
 X_train, X_test, y_train, y_test = model_selection.train_test_split(feature_matrix, target, test_size=0.2, random_state=1)
+
+### Scaling for Lasso
+scaler = StandardScaler()
+train_scaled = scaler.fit_transform(X_train)
+test_scaled = scaler.transform(X_test)
+train_scaled_df = pd.DataFrame(train_scaled, columns = X_train.columns)
+test_scaled_df = pd.DataFrame(test_scaled, columns = X_test.columns)
+
+### XGBoost datasets
 train = xgb.DMatrix(data=X_train, label=y_train)
 test = xgb.DMatrix(data=X_test, label=y_test)
 
@@ -43,38 +55,54 @@ print('Prevalence y train:', round(sum(y_train) / len(y_train), 4))
 print('Prevalence y test:', round(sum(y_test) / len(y_test), 4))
 
 
-# In[4]:
+# In[9]:
 
 
-# Import model
+# Import models
 xgb_clf = jb.load('C:\\Users\\Megaport\\Desktop\\jupyterNotebook\\grid_search\\optimal_xgb.joblib')
+en_clf = jb.load('C:\\Users\\Megaport\\Desktop\\jupyterNotebook\\grid_search\\optimal_model_en.joblib')
 
 
-# In[5]:
+# In[16]:
 
 
-# SHAP
-explainer =  shap.TreeExplainer(xgb_clf)
-get_ipython().run_line_magic('time', 'shap_values = explainer.shap_values(test)')
+# SHAP en
+explainer_en = shap.LinearExplainer(en_clf, train_scaled, link=shap.links.logit)
+get_ipython().run_line_magic('time', 'shap_values_en = explainer_en.shap_values(test_scaled)')
 
 
-# In[6]:
+# In[17]:
 
 
-shap_values[1]
+# SHAP xgb
+explainer_xgb =  shap.TreeExplainer(xgb_clf)
+get_ipython().run_line_magic('time', 'shap_values_xgb = explainer_xgb.shap_values(test)')
 
 
-# In[27]:
+# In[18]:
 
 
-### SHAP plot - Relation of variables
-shap.summary_plot(shap_values, X_test, plot_type='dot')
+shap_values_en[1]
+
+
+# In[24]:
+
+
+### SHAP plot en - Relation of variables
+shap.summary_plot(shap_values_en, test_scaled_df, plot_type='dot')
+
+
+# In[22]:
+
+
+### SHAP plot xgb - Relation of variables
+shap.summary_plot(shap_values_xgb, X_test, plot_type='dot')
 
 
 # In[29]:
 
 
-### SHAP plot - Relation target with 2 variables
+### SHAP plot xgb - Relation target with 2 variables
 shap.dependence_plot("ageMeanConductors", shap_values, X_test, interaction_index= 'sexe_female_conductor_1')
 
 
