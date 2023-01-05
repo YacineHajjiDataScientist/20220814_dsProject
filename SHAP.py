76bec5c2b0f50b01c552b93eb4e2c9749a3c8580
YacineHajjiDataScientist <111184319+YacineHajjiDataScientist@users.noreply.github.com>
2023-01-05 21:09:14 +0100

    Add files via upload

--- SHAP.py
+++ SHAP.py
@@ -264,7 +264,7 @@ sage_values.plot(feature_names)
 
 # ### Predictions with XGBoost
 
-# In[39]:
+# In[5]:
 
 
 ### Import libraries
@@ -291,7 +291,7 @@ xgb_preds_test_bin07 = np.where(xgb_preds_test >= 0.7, 1, 0)
 # In[18]:
 
 
-### Train contingency table at 0.3 cutoff
+### Train contingency table at 0.5 cutoff
 pd.crosstab(y_train, xgb_preds_train_bin05, colnames=['xgb_pred_train'], normalize=True)
 
 
@@ -343,14 +343,14 @@ print(classification_report(y_test.astype('int'), xgb_preds_test_bin03))
 print(classification_report(y_test.astype('int'), xgb_preds_test_bin05))
 
 
-# In[41]:
+# In[24]:
 
 
 ### Performance criteria at 0.7 cutoff
 print(classification_report(y_test.astype('int'), xgb_preds_test_bin07))
 
 
-# In[54]:
+# In[251]:
 
 
 ### Function defining Youden index
@@ -360,24 +360,26 @@ def cutoff_youdens_j(fpr_xgb, tpr_xgb, seuils):
     return j_ordered[-1][1]
 
 ### Finding Youden cutoff
-youden_cutoff = cutoff_youdens_j(fpr_xgb, tpr_xgb, seuils)
+youden_cutoff_xgb = cutoff_youdens_j(fpr_xgb, tpr_xgb, seuils)
 
 ### Performance criteria at Youden cutoff
-print(classification_report(y_test.astype('int'), np.where(xgb_preds_test >= youden_cutoff, 1, 0)))
-print('Youden index=', round(youden_cutoff, 2))
+print(classification_report(y_test.astype('int'), np.where(xgb_preds_test >= youden_cutoff_xgb, 1, 0)))
+print('Youden index=', round(youden_cutoff_xgb, 2))
 
 
 # ##### ROC curve
 
-# 
-
-# In[67]:
+# In[249]:
 
 
 ### AUC
 fpr_xgb, tpr_xgb, seuils = roc_curve(y_test.astype('int'), xgb_preds_test, pos_label=1)
 roc_auc_xgb = auc(fpr_xgb, tpr_xgb)
 roc_auc_xgb
+
+
+# In[154]:
+
 
 ### ROC curve
 plt.figure(figsize=(6, 6))
@@ -391,4 +393,252 @@ plt.annotate('High positive recall (0.3)', xy=(1-0.57, 0.82), xytext=(1-0.47, 0.
 plt.annotate('Standard cutoff (0.5)', xy=(1-0.84, 0.60), xytext=(1-0.74, 0.59), arrowprops={'facecolor' : 'grey'})
 plt.annotate('High negative recall (0.7)', xy=(1-0.94, 0.40), xytext=(1-0.84, 0.39), arrowprops={'facecolor' : '#76D680'})
 plt.annotate('Youden index (0.43)', xy=(1-0.78, 0.67), xytext=(1-0.68, 0.66), arrowprops={'facecolor' : '#76C3D6'});
+
+
+# ##### Finding optimal cutoff for each model
+# ##### 1- XGBoost
+
+# In[22]:
+
+
+### Performances at each cutoff
+# Initiating DataFrame
+df_cutoff_recall_precision_xgb = pd.DataFrame({'seuils':seuils, 'fpr_xgb':fpr_xgb, 'tpr_xgb':tpr_xgb})
+
+# Filling DataFrame through loop
+nb_neg_pred_neg = []
+nb_pos_pred_neg = []
+nb_neg_pred_pos = []
+nb_pos_pred_pos = []
+for i in df_cutoff_recall_precision_xgb['seuils']:
+    nb_neg_pred_neg.append(np.logical_and(xgb_preds_test<i , y_test.astype('int')==0).sum())
+    nb_pos_pred_neg.append(np.logical_and(xgb_preds_test<i , y_test.astype('int')==1).sum())
+    nb_neg_pred_pos.append(np.logical_and(xgb_preds_test>=i , y_test.astype('int')==0).sum())
+    nb_pos_pred_pos.append(np.logical_and(xgb_preds_test>=i , y_test.astype('int')==1).sum())
+
+# Filling DataFrame with remaining estimands
+df_cutoff_recall_precision_xgb['nb_neg_pred_neg'] = nb_neg_pred_neg
+df_cutoff_recall_precision_xgb['nb_pos_pred_neg'] = nb_pos_pred_neg
+df_cutoff_recall_precision_xgb['nb_neg_pred_pos'] = nb_neg_pred_pos
+df_cutoff_recall_precision_xgb['nb_pos_pred_pos'] = nb_pos_pred_pos
+df_cutoff_recall_precision_xgb['nb_neg'] = sum(y_test.astype('int')==0)
+df_cutoff_recall_precision_xgb['nb_pos'] = sum(y_test.astype('int')==1)
+df_cutoff_recall_precision_xgb['nb_pred_neg'] = df_cutoff_recall_precision_xgb['nb_neg_pred_neg'] + df_cutoff_recall_precision_xgb['nb_pos_pred_neg']
+df_cutoff_recall_precision_xgb['nb_pred_pos'] = df_cutoff_recall_precision_xgb['nb_neg_pred_pos'] + df_cutoff_recall_precision_xgb['nb_pos_pred_pos']
+
+# Computing performance criteria at each threshold
+df_cutoff_recall_precision_xgb['precision_0'] = df_cutoff_recall_precision_xgb['nb_neg_pred_neg'] / df_cutoff_recall_precision_xgb['nb_pred_neg']
+df_cutoff_recall_precision_xgb['precision_1'] = df_cutoff_recall_precision_xgb['nb_pos_pred_pos'] / df_cutoff_recall_precision_xgb['nb_pred_pos']
+df_cutoff_recall_precision_xgb['recall_0'] = df_cutoff_recall_precision_xgb['nb_neg_pred_neg'] / df_cutoff_recall_precision_xgb['nb_neg']
+df_cutoff_recall_precision_xgb['recall_1'] = df_cutoff_recall_precision_xgb['nb_pos_pred_pos'] / df_cutoff_recall_precision_xgb['nb_pos']
+
+# f1 scores
+df_cutoff_recall_precision_xgb['f1_0'] = 2 * (df_cutoff_recall_precision_xgb['precision_0'] * df_cutoff_recall_precision_xgb['recall_0']) / (df_cutoff_recall_precision_xgb['precision_0'] + df_cutoff_recall_precision_xgb['recall_0'])
+df_cutoff_recall_precision_xgb['f1_1'] = 2 * (df_cutoff_recall_precision_xgb['precision_1'] * df_cutoff_recall_precision_xgb['recall_1']) / (df_cutoff_recall_precision_xgb['precision_1'] + df_cutoff_recall_precision_xgb['recall_1'])
+
+
+# In[119]:
+
+
+df_cutoff_recall_precision_xgb
+
+
+# In[141]:
+
+
+### Reducing number of columns
+df_summary_xgb = round(df_cutoff_recall_precision_xgb[['seuils', 'precision_0', 'precision_1', 'recall_0', 'recall_1', 'f1_0', 'f1_1']], 2)
+
+
+# In[157]:
+
+
+### Verification of results
+df_summary_xgb.loc[round(df_summary_xgb.seuils, 6) == 0.70].iloc[1]
+
+
+# ##### 2- Elastic Net
+
+# In[163]:
+
+
+### Predictions
+en_preds_test = en_clf.predict(test_scaled_df)
+
+### AUC
+fpr_en, tpr_en, seuils = roc_curve(y_test.astype('int'), en_preds_test, pos_label=1)
+roc_auc_en = auc(fpr_en, tpr_en)
+roc_auc_en
+
+### Performances at each cutoff
+# Initiating DataFrame
+df_cutoff_recall_precision_en = pd.DataFrame({'seuils':seuils, 'fpr_en':fpr_en, 'tpr_en':tpr_en})
+
+# Filling DataFrame through loop
+nb_neg_pred_neg = []
+nb_pos_pred_neg = []
+nb_neg_pred_pos = []
+nb_pos_pred_pos = []
+for i in df_cutoff_recall_precision_en['seuils']:
+    nb_neg_pred_neg.append(np.logical_and(en_preds_test<i , y_test.astype('int')==0).sum())
+    nb_pos_pred_neg.append(np.logical_and(en_preds_test<i , y_test.astype('int')==1).sum())
+    nb_neg_pred_pos.append(np.logical_and(en_preds_test>=i , y_test.astype('int')==0).sum())
+    nb_pos_pred_pos.append(np.logical_and(en_preds_test>=i , y_test.astype('int')==1).sum())
+
+# Filling DataFrame with remaining estimands
+df_cutoff_recall_precision_en['nb_neg_pred_neg'] = nb_neg_pred_neg
+df_cutoff_recall_precision_en['nb_pos_pred_neg'] = nb_pos_pred_neg
+df_cutoff_recall_precision_en['nb_neg_pred_pos'] = nb_neg_pred_pos
+df_cutoff_recall_precision_en['nb_pos_pred_pos'] = nb_pos_pred_pos
+df_cutoff_recall_precision_en['nb_neg'] = sum(y_test.astype('int')==0)
+df_cutoff_recall_precision_en['nb_pos'] = sum(y_test.astype('int')==1)
+df_cutoff_recall_precision_en['nb_pred_neg'] = df_cutoff_recall_precision_en['nb_neg_pred_neg'] + df_cutoff_recall_precision_en['nb_pos_pred_neg']
+df_cutoff_recall_precision_en['nb_pred_pos'] = df_cutoff_recall_precision_en['nb_neg_pred_pos'] + df_cutoff_recall_precision_en['nb_pos_pred_pos']
+
+# Computing performance criteria at each threshold
+df_cutoff_recall_precision_en['precision_0'] = df_cutoff_recall_precision_en['nb_neg_pred_neg'] / df_cutoff_recall_precision_en['nb_pred_neg']
+df_cutoff_recall_precision_en['precision_1'] = df_cutoff_recall_precision_en['nb_pos_pred_pos'] / df_cutoff_recall_precision_en['nb_pred_pos']
+df_cutoff_recall_precision_en['recall_0'] = df_cutoff_recall_precision_en['nb_neg_pred_neg'] / df_cutoff_recall_precision_en['nb_neg']
+df_cutoff_recall_precision_en['recall_1'] = df_cutoff_recall_precision_en['nb_pos_pred_pos'] / df_cutoff_recall_precision_en['nb_pos']
+
+# f1 scores
+df_cutoff_recall_precision_en['f1_0'] = 2 * (df_cutoff_recall_precision_en['precision_0'] * df_cutoff_recall_precision_en['recall_0']) / (df_cutoff_recall_precision_en['precision_0'] + df_cutoff_recall_precision_en['recall_0'])
+df_cutoff_recall_precision_en['f1_1'] = 2 * (df_cutoff_recall_precision_en['precision_1'] * df_cutoff_recall_precision_en['recall_1']) / (df_cutoff_recall_precision_en['precision_1'] + df_cutoff_recall_precision_en['recall_1'])
+
+### Reducing number of columns
+df_summary_en = round(df_cutoff_recall_precision_en[['seuils', 'precision_0', 'precision_1', 'recall_0', 'recall_1', 'f1_0', 'f1_1']], 2)
+
+
+# In[252]:
+
+
+### Function defining Youden index
+def cutoff_youdens_j(fpr_en, tpr_en, seuils):
+    j_scores = tpr_en - fpr_en
+    j_ordered = sorted(zip(j_scores, seuils))
+    return j_ordered[-1][1]
+
+### Finding Youden cutoff
+youden_cutoff_en = cutoff_youdens_j(fpr_en, tpr_en, seuils)
+
+### Performance criteria at Youden cutoff
+print(classification_report(y_test.astype('int'), np.where(en_preds_test >= youden_cutoff_en, 1, 0)))
+print('Youden index=', round(youden_cutoff_en, 2))
+
+
+# ##### 3- LGBM
+
+# In[175]:
+
+
+### Predictions
+lgbm_preds_test = lgbm_clf.predict(test_scaled_df)
+
+### AUC
+fpr_lgbm, tpr_lgbm, seuils = roc_curve(y_test.astype('int'), lgbm_preds_test, pos_label=1)
+roc_auc_lgbm = auc(fpr_lgbm, tpr_lgbm)
+roc_auc_lgbm
+
+### Performances at each cutoff
+# Initiating DataFrame
+df_cutoff_recall_precision_lgbm = pd.DataFrame({'seuils':seuils, 'fpr_lgbm':fpr_lgbm, 'tpr_lgbm':tpr_lgbm})
+
+# Filling DataFrame through loop
+nb_neg_pred_neg = []
+nb_pos_pred_neg = []
+nb_neg_pred_pos = []
+nb_pos_pred_pos = []
+for i in df_cutoff_recall_precision_lgbm['seuils']:
+    nb_neg_pred_neg.append(np.logical_and(lgbm_preds_test<i , y_test.astype('int')==0).sum())
+    nb_pos_pred_neg.append(np.logical_and(lgbm_preds_test<i , y_test.astype('int')==1).sum())
+    nb_neg_pred_pos.append(np.logical_and(lgbm_preds_test>=i , y_test.astype('int')==0).sum())
+    nb_pos_pred_pos.append(np.logical_and(lgbm_preds_test>=i , y_test.astype('int')==1).sum())
+
+# Filling DataFrame with remaining estimands
+df_cutoff_recall_precision_lgbm['nb_neg_pred_neg'] = nb_neg_pred_neg
+df_cutoff_recall_precision_lgbm['nb_pos_pred_neg'] = nb_pos_pred_neg
+df_cutoff_recall_precision_lgbm['nb_neg_pred_pos'] = nb_neg_pred_pos
+df_cutoff_recall_precision_lgbm['nb_pos_pred_pos'] = nb_pos_pred_pos
+df_cutoff_recall_precision_lgbm['nb_neg'] = sum(y_test.astype('int')==0)
+df_cutoff_recall_precision_lgbm['nb_pos'] = sum(y_test.astype('int')==1)
+df_cutoff_recall_precision_lgbm['nb_pred_neg'] = df_cutoff_recall_precision_lgbm['nb_neg_pred_neg'] + df_cutoff_recall_precision_lgbm['nb_pos_pred_neg']
+df_cutoff_recall_precision_lgbm['nb_pred_pos'] = df_cutoff_recall_precision_lgbm['nb_neg_pred_pos'] + df_cutoff_recall_precision_lgbm['nb_pos_pred_pos']
+
+# Computing performance criteria at each threshold
+df_cutoff_recall_precision_lgbm['precision_0'] = df_cutoff_recall_precision_lgbm['nb_neg_pred_neg'] / df_cutoff_recall_precision_lgbm['nb_pred_neg']
+df_cutoff_recall_precision_lgbm['precision_1'] = df_cutoff_recall_precision_lgbm['nb_pos_pred_pos'] / df_cutoff_recall_precision_lgbm['nb_pred_pos']
+df_cutoff_recall_precision_lgbm['recall_0'] = df_cutoff_recall_precision_lgbm['nb_neg_pred_neg'] / df_cutoff_recall_precision_lgbm['nb_neg']
+df_cutoff_recall_precision_lgbm['recall_1'] = df_cutoff_recall_precision_lgbm['nb_pos_pred_pos'] / df_cutoff_recall_precision_lgbm['nb_pos']
+
+# f1 scores
+df_cutoff_recall_precision_lgbm['f1_0'] = 2 * (df_cutoff_recall_precision_lgbm['precision_0'] * df_cutoff_recall_precision_lgbm['recall_0']) / (df_cutoff_recall_precision_lgbm['precision_0'] + df_cutoff_recall_precision_lgbm['recall_0'])
+df_cutoff_recall_precision_lgbm['f1_1'] = 2 * (df_cutoff_recall_precision_lgbm['precision_1'] * df_cutoff_recall_precision_lgbm['recall_1']) / (df_cutoff_recall_precision_lgbm['precision_1'] + df_cutoff_recall_precision_lgbm['recall_1'])
+
+### Reducing number of columns
+df_summary_lgbm = round(df_cutoff_recall_precision_lgbm[['seuils', 'precision_0', 'precision_1', 'recall_0', 'recall_1', 'f1_0', 'f1_1']], 2)
+
+
+# ##### Comparison of models
+
+# ##### 1- XGBoost
+
+# In[173]:
+
+
+### Optimal points to get 80% recall positif or 80% precision positive
+df_summary_xgb.loc[(round(df_cutoff_recall_precision_xgb.recall_1, 2)==0.8) | (round(df_cutoff_recall_precision_xgb.precision_1, 2)==0.8)].iloc[[0, -1]]
+
+
+# In[302]:
+
+
+### Youden index XGBoost
+print('Youden index=', round(youden_cutoff_xgb, 2))
+round(pd.DataFrame(classification_report(y_test.astype('int'), np.where(xgb_preds_test >= youden_cutoff_xgb, 1, 0), output_dict=True)).transpose()[['precision', 'recall', 'f1-score']].loc[['0', '1']], 2)
+
+
+# ##### 2- Elastic Net
+
+# In[172]:
+
+
+### Optimal points to get 80% recall positif or 80% precision positive
+df_summary_en.loc[(round(df_cutoff_recall_precision_en.recall_1, 2)==0.8) | (round(df_cutoff_recall_precision_en.precision_1, 2)==0.8)].iloc[[0, -1]]
+
+
+# In[303]:
+
+
+### Youden index Elastic Net
+print('Youden index=', round(youden_cutoff_en, 2))
+round(pd.DataFrame(classification_report(y_test.astype('int'), np.where(en_preds_test >= youden_cutoff_en, 1, 0), output_dict=True)).transpose()[['precision', 'recall', 'f1-score']].loc[['0', '1']], 2)
+
+
+# In[287]:
+
+
+### ROC curves
+plt.figure(figsize=(6, 6))
+# High positive recall
+plt.plot(1-0.617, 0.8, 'x', color='#D67676', markersize=10, mew=3)
+plt.plot(1-0.55, 0.8, 'x', color='#D67676', markersize=10, mew=3)
+# High positive precision
+plt.plot(1-0.925, 0.45, 'x', color='#76D680', markersize=10, mew=3)
+plt.plot(1-0.93, 0.4, 'x', color='#76D680', markersize=10, mew=3)
+# Youden
+plt.plot(1-0.78, 0.67, 'x', color='#76C3D6', markersize=10, mew=3)
+plt.plot(1-0.77, 0.64, 'x', color='#76C3D6', markersize=10, mew=3)
+# ROC
+plt.plot(fpr_xgb, tpr_xgb, color='black', lw=2, label=['XGBoost', round(roc_auc_xgb, 2)])
+plt.plot(fpr_en, tpr_en, color='grey', lw=2, label=['elastic net', round(roc_auc_en, 2)])
+plt.plot(np.arange(0, 1, 0.01), np.arange(0, 1, 0.01), 'b--', label=['Reference', '0.50'])
+plt.ylabel('Taux vrais positifs')
+plt.xlabel('Taux faux positifs')
+plt.title('Courbes ROC')
+plt.legend(loc='lower right')
+plt.annotate('High positive precision (0.8)', xy=(1-0.9, 0.425), xytext=(1-0.9, 0.425))
+plt.annotate('High positive recall (0.8)', xy=(1-0.52, 0.785), xytext=(1-0.52, 0.785))
+plt.annotate('Youden index', xy=(1-0.74, 0.65), xytext=(1-0.74, 0.65));
+
+# Carefull, precision is not monotone
 
